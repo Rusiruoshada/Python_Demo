ad7da46ba12d24509ad5911d025bac44ae5d8916
Rusiruoshada <rusirusamaraweera749@gmail.com>
2026-03-04 17:27:15 +0530

    make progress with string

---
+++ basics/string.py
@@ -0,0 +1,19 @@
+stringText = """
+    we can use \"\"\"<text> \"\"\" to do multi line string. also this can be.
+    we use \\ as a escape character
+    comment can do by using #.
+    string concatenation done by +. it can use for addition too
+"""
+print(stringText)
+
+exampleText = 'this is a string!'
+
+# string index start from 0 and when we gave a range its start from 0 if we gave negative values
+print(exampleText[2]) # output : i
+print(exampleText[:6]) # output :this i
+print(exampleText[3:]) # output : s is a string!
+print(exampleText[-3]) # output : n
+print(exampleText[-1:-4]) # output : no input
+print(exampleText[-2:4]) # output : no input
+print(exampleText[2:-4]) # output : is is a str
+
