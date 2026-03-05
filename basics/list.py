f786560690b54028e39e2ebd7ed53148b8d6355b
Rusiruoshada <rusirusamaraweera749@gmail.com>
2026-03-05 15:09:41 +0530

    learning list in python

---
+++ basics/list.py
@@ -0,0 +1,16 @@
+num = [2,3,4,5,6,7,4,2,45,4]
+num1 = ['nav',32,3.23]
+mix = [num,num1]
+
+#num slice
+print(num[2:5])
+
+print(num.append(33)) # add value to end
+print(num.count(4)) # count how many appear in the list
+print(num.insert(1,9)) # add the value in given index (index, value)
+print(num.remove(45))
+print(num.pop(3)) # pop work like stack(index 0), pop given index
+
+
+
+
