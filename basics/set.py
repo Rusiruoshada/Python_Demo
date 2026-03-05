36e8356f9b53eac0ed27d27b53840d48bcb5f2a2
Rusiruoshada <rusirusamaraweera749@gmail.com>
2026-03-05 17:21:43 +0530

    practice set in python

---
+++ basics/set.py
@@ -0,0 +1,9 @@
+#  set is a collection of unique unordered ele
+# set not store base on the value its use hash value
+# use {} to sets
+
+num = {12,3,'hel',90,3}
+
+print(num) # only show unique values so , end 3 not showing -> {90, 3, 12, 'hel'}
+print(type(num)) # <class 'set'>
+
