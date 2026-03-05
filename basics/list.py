581284788a0ece9c01d7492b9c96062e73c3566f
Rusiruoshada <rusirusamaraweera749@gmail.com>
2026-03-05 15:20:18 +0530

    test list in build functions

--- basics/list.py
+++ basics/list.py
@@ -6,11 +6,22 @@ mix = [num,num1]
 print(num[2:5])
 
 print(num.append(33)) # add value to end
+print(num)
 print(num.count(4)) # count how many appear in the list
-print(num.insert(1,9)) # add the value in given index (index, value)
-print(num.remove(45))
+num.insert(1,9) # add the value in given index (index, value)
+print(num)
+num.remove(45)
+print(num)
 print(num.pop(3)) # pop work like stack(index 0), pop given index
-
-
-
-
+del num[7:] # use del to delete multiple values in list
+print(num)
+num.extend([10,11,12,121])
+print(num) # use extend to add more element at once at the end
+num.reverse()
+print(num)
+num.sort() # default ASC
+print(num)
+print(min(num))
+print(max(num))
+print(sum(num)) # get the sum
+print(type(num)) # <class 'list'>
