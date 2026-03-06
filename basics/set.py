30316cf979a4ffcf7f6965994ce331cb3db0ea84
Rusiruoshada <rusirusamaraweera749@gmail.com>
2026-03-06 11:38:40 +0530

    learn set new concepts when we use relation algebra like union, set deferent, common value,..

--- basics/set.py
+++ basics/set.py
@@ -7,3 +7,20 @@ num = {12,3,'hel',90,3}
 print(num) # only show unique values so , end 3 not showing -> {90, 3, 12, 'hel'}
 print(type(num)) # <class 'set'>
 
+# creating a empty set
+a = {} # this will show as type dict(dictionary)
+a1 = set()
+
+print(type(a))
+print(type(a1))
+
+set1 = set('abcdmnop')
+set2 = set('aeioupd')
+
+print(set1)
+print(set2)
+
+print(set1 - set2) # show only in set1
+print(set1 | set2) # all value
+print(set1 & set2) # common values
+print(set1 ^ set2) # exclude common value only show set1 and set2 unique value
