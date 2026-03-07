08ec482924fa0fb532995d5c859fa840e42f948a
Rusiruoshada <rusirusamaraweera749@gmail.com>
2026-03-07 12:55:11 +0530

    learn function

---
+++ basics/function.py
@@ -0,0 +1,6 @@
+# to create function we use def keyword
+# in python we don't use {} to show scope of a function we use indentation(tab or space)
+def add(a,b) :
+    return a*b
+
+print(add(10,12))
