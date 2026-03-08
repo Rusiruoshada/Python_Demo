bb340d6499f6ca1a8abb82c1f662fd96a48ddb98
Rusiruoshada <rusirusamaraweera749@gmail.com>
2026-03-08 13:03:12 +0530

    learn about python loop while and for

---
+++ basics/loops.py
@@ -0,0 +1,34 @@
+# python mainly have while and for loop
+# for loop work as for each(no condition check)
+# while
+a = 10
+while a<20:
+    print('use while loop');
+    a +=1
+
+# nested while
+b = 0;
+while b<10:
+    # in print method its have end parameter its usually set to '\n' (new line). we can change it
+    print("hello", end = ' ')
+    j=0
+    while j<5:
+        print(" o kawai koto!",end=' ')
+        j += 1
+    print()
+    b +=1
+
+# for loop
+c = [2,'roms',3.2,None,True,'python']
+cSize = len(c)
+
+print('',end='[')
+for val in c:
+    print(val,end=', ')
+
+print('',end=']\n')
+# output: [2, roms, 3.2, None, True, python, ]
+
+
+
+
